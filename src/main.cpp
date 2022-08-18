ac22369df1f2b210c8f59d382e4619c23db83c4b
EDDragonWolf <eddragonwolf@urk.net>
2022-08-18 05:41:04 +0000

    Added implementation for Canny filter

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #pragma warning(disable : 4819)
 #endif
 
-// #include <Exceptions.h>
+#include <Exceptions.h>
 #include <ImageIO.h>
 // #include <ImagesCPU.h>
 // #include <ImagesNPP.h>
@@ -42,6 +42,76 @@ bool printfNPPinfo(int argc, char *argv[])
     return bVal;
 }
 
+void cannyFilter(const std::string &filePath, const std::string &outputFile)
+{
+    try
+    {
+        std::cout << "Processing of " << filePath << " started." << std::endl;
+        npp::ImageCPU_8u_C1 hostSrc;
+        npp::loadImage(filePath, hostSrc);
+        npp::ImageNPP_8u_C1 deviceSrc(hostSrc);
+        const NppiSize srcSize = {(int)deviceSrc.width(), (int)deviceSrc.height()};
+        const NppiPoint srcOffset = {0, 0};
+
+        const NppiSize filterROI = {(int)deviceSrc.width(), (int)deviceSrc.height()};
+        npp::ImageNPP_8u_C1 deviceDst(filterROI.width, filterROI.height);
+
+        const Npp16s lowThreshold = 72;
+        const Npp16s highThreshold = 256;
+
+        int bufferSize = 0;
+        Npp8u *scratchBufferNPP = 0;
+        NPP_CHECK_NPP(nppiFilterCannyBorderGetBufferSize(filterROI, &bufferSize));
+        cudaMalloc((void **)&scratchBufferNPP, bufferSize);
+
+        if ((bufferSize > 0) && (scratchBufferNPP != 0))
+        {
+            NPP_CHECK_NPP(nppiFilterCannyBorder_8u_C1R(deviceSrc.data(), deviceSrc.pitch(), srcSize, srcOffset,
+                                                       deviceDst.data(), deviceDst.pitch(), filterROI,
+                                                       NppiDifferentialKernel::NPP_FILTER_SOBEL, NppiMaskSize::NPP_MASK_SIZE_3_X_3,
+                                                       lowThreshold, highThreshold, nppiNormL2, NPP_BORDER_REPLICATE,
+                                                       scratchBufferNPP));
+        }
+
+        cudaFree(scratchBufferNPP);
+
+        npp::ImageCPU_8u_C1 hostDst(deviceDst.size());
+        deviceDst.copyTo(hostDst.data(), hostDst.pitch());
+        saveImage(outputFile, hostDst);
+        std::cout << "Processing of " << filePath << " ended. Result saved to: " << outputFile << std::endl;
+
+        nppiFree(deviceSrc.data());
+        nppiFree(deviceDst.data());
+    }
+    catch (npp::Exception &rException)
+    {
+        std::cerr << "Program error! The following exception occurred: \n";
+        std::cerr << rException << std::endl;
+        std::cerr << "Aborting." << std::endl;
+
+        exit(EXIT_FAILURE);
+    }
+    catch (...)
+    {
+        std::cerr << "Program error! An unknow type of exception occurred. \n";
+        std::cerr << "Aborting." << std::endl;
+
+        exit(EXIT_FAILURE);
+    }
+}
+
+void applyFilter(const std::string &filterType, const std::string &filePath, const std::string &outputFile)
+{
+    if (filterType == "canny")
+    {
+        cannyFilter(filePath, outputFile);
+    }
+    else
+    {
+        std::cout << "Filter type isn't supported!" << std::endl;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     printf("%s Starting...\n\n", argv[0]);
@@ -54,9 +124,9 @@ int main(int argc, char *argv[])
     findCudaDevice(argc, (const char **)argv);
 
     char *inputData;
-    if (checkCmdLineFlag(argc, (const char **)argv, "input"))
+    if (argc >= 2)
     {
-        getCmdLineArgumentString(argc, (const char **)argv, "input", &inputData);
+        inputData = argv[1];
         if (!inputData)
         {
             std::cerr << "Cannot read the input data!" << std::endl;
@@ -69,6 +139,41 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
+    char *filterData;
+    if (argc >= 3)
+    {
+        filterData = argv[2];
+        if (!inputData)
+        {
+            std::cerr << "Cannot read the filter type!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        std::cerr << "Filter type is missed!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    char *outputData;
+    if (argc >= 4)
+    {
+        outputData = argv[3];
+        if (!inputData)
+        {
+            std::cerr << "Cannot read the filter type!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        std::cerr << "Filter type is missed!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::string filterType{filterData};
+    std::string output{outputData};
+
     std::string inputValue{inputData};
     if (std::filesystem::is_directory(inputValue))
     {
@@ -76,52 +181,13 @@ int main(int argc, char *argv[])
     }
     else
     {
-        
+        std::string outputFile{output};
+        if (std::filesystem::is_directory(output))
+        {
+            outputFile += "/" + std::filesystem::path(inputValue).filename().string();
+        }
+        applyFilter(filterType, inputValue, outputFile);
     }
-    
-
-    //     try
-    //     {
-    //         std::string sFilename;
-    //         char *filePath;
-
-    //         findCudaDevice(argc, (const char **)argv);
-
-
-
-    //         if (filePath)
-    //         {
-    //             sFilename = filePath;
-    //         }
-    //         else
-    //         {
-    //             sFilename = "Lena.pgm";
-    //         }
-
-    //         // if we specify the filename at the command line, then we only test
-    //         // sFilename[0].
-    //         int file_errors = 0;
-    //         std::ifstream infile(sFilename.data(), std::ifstream::in);
-
-    //         if (infile.good())
-    //         {
-    //             std::cout << "boxFilterNPP opened: <" << sFilename.data()
-    //                       << "> successfully!" << std::endl;
-    //             file_errors = 0;
-    //             infile.close();
-    //         }
-    //         else
-    //         {
-    //             std::cout << "boxFilterNPP unable to open: <" << sFilename.data() << ">"
-    //                       << std::endl;
-    //             file_errors++;
-    //             infile.close();
-    //         }
-
-    //         if (file_errors > 0)
-    //         {
-    //             exit(EXIT_FAILURE);
-    //         }
 
     //         std::string sResultFilename = sFilename;
 
@@ -142,33 +208,9 @@ int main(int argc, char *argv[])
     //             sResultFilename = outputFilePath;
     //         }
 
-    //         // declare a host image object for an 8-bit grayscale image
-    //         npp::ImageCPU_8u_C1 oHostSrc;
-    //         // load gray-scale image from disk
-    //         npp::loadImage(sFilename, oHostSrc);
-    //         // declare a device image and copy construct from the host image,
-    //         // i.e. upload host to device
-    //         npp::ImageNPP_8u_C1 oDeviceSrc(oHostSrc);
 
-    //         // create struct with box-filter mask size
-    //         NppiSize oMaskSize = {5, 5};
 
-    //         NppiSize oSrcSize = {(int)oDeviceSrc.width(), (int)oDeviceSrc.height()};
-    //         NppiPoint oSrcOffset = {0, 0};
 
-    //         // create struct with ROI size
-    //         NppiSize oSizeROI = {(int)oDeviceSrc.width(), (int)oDeviceSrc.height()};
-    //         // allocate device image of appropriately reduced size
-    //         npp::ImageNPP_8u_C1 oDeviceDst(oSizeROI.width, oSizeROI.height);
-    //         // set anchor point inside the mask to (oMaskSize.width / 2,
-    //         // oMaskSize.height / 2) It should round down when odd
-    //         NppiPoint oAnchor = {oMaskSize.width / 2, oMaskSize.height / 2};
-
-    //         // run box filter
-    //         NPP_CHECK_NPP(nppiFilterBoxBorder_8u_C1R(
-    //             oDeviceSrc.data(), oDeviceSrc.pitch(), oSrcSize, oSrcOffset,
-    //             oDeviceDst.data(), oDeviceDst.pitch(), oSizeROI, oMaskSize, oAnchor,
-    //             NPP_BORDER_REPLICATE));
 
     //         // declare a host image for the result
     //         npp::ImageCPU_8u_C1 oHostDst(oDeviceDst.size());
